d5faaab4bab2678687f3e1023769ea5d5b225b75
Ezra Muskat <ezramuskat@gmail.com>
2020-03-13 13:29:09 +0200

    Turned code directory into package and added pymongo setup

--- code/top_sort.py
+++ code/top_sort.py
@@ -1,4 +1,10 @@
 from code.class_node import Class_Node
+import pymongo
+
+# TODO - replace default values with actual values for the database
+client = pymongo.MongoClient('hostname', 27017) 
+
+db = client['database_name']
 
 def top_sort():
     pass
